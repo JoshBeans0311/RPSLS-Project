08b2ac7e1ec814da1192abc717c0028d080298b7
Joshua Beans <Joshbeans0311@gmail.com>
2020-08-20 17:42:21 -0500

    Init

---
+++ rpsls.js
@@ -0,0 +1,76 @@
+"use strict";
+
+
+// single player with random "AI" or multiplayer with only two players (make this a choice at the beginning)
+// incorporate inheritance (super class type player, sub class Human/Computer)
+// create array of gestures
+// make games minimum best of 3
+// validate bad input
+
+// extra
+// account for case validation
+// possible different modes
+// possible different oponents with varying skills
+
+
+// start with player vs player then automate "player 2"
+
+// classes - player, computer, weapon, game
+
+class Game (
+    constructor() {
+
+    }
+)
+        
+class Player (
+    constructor() {
+        this.score
+    }
+)
+
+class Computer (
+    constructor() {
+
+    }
+)
+
+class Weapon (
+    constructor() {
+
+    }
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+
+// // outputs
+// Rock crushes Scissors  
+// Scissors cuts Paper 
+// Paper covers Rock 
+// Rock crushes Lizard 
+// Lizard poisons Spock 
+// Spock smashes Scissors 
+// Scissors decapitates Lizard 
+// Lizard eats Paper 
+// Paper disproves Spock 
+// Spock vaporizes Rock  
