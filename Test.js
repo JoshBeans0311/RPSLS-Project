1c55c994512fb6ea23ae07cf911a9f14941982f3
Joshua Beans <Joshbeans0311@gmail.com>
2020-08-25 19:28:04 -0500

    initial framework created, not functioning

---
+++ Test.js
@@ -0,0 +1,162 @@
+"use strict";
+
+
+// single player with random "AI" or multiplayer with only two players (make this a choice at the beginning)
+// incorporate inheritance (super class type player, sub class Human/Computer)
+// create array of gestures
+// make games minimum best of 3
+// validate bad input
+
+// extra
+// account for case validation
+// possible different modes
+// possible different oponents with varying skills
+
+
+// start with player vs player then automate "player 2"
+
+
+
+
+class Game{
+
+
+
+
+    
+
+}
+
+class Player{
+    constructor() {
+        this.
+
+
+    }
+
+
+
+}
+
+class Computer{
+    constructor() {
+        this.name = computer;
+
+
+    }
+
+
+
+
+}
+
+class Weapon {
+        
+    constructor() {
+        this.name = weapon;
+    
+        let rockWeapon = new Weapon("Rock");
+        let paperWeapon = new Weapon("Paper");
+        let scissorsWeapon = new Weapon("Scissors");
+        let lizzardWeapon = new Weapon("Lizzard");
+        let spockWeapon = new Weapon("Spock");
+
+    
+    
+    }
+
+
+
+displayInformation(){
+    console.log(this.name);
+
+}
+
+
+
+
+
+
+
+        
+            
+/////////////////////////////////////////////////////////////////
+
+
+class Weapon {
+        
+    constructor() {
+        this.name = weapon;
+    
+        let rockWeapon = new Weapon("Rock");
+        let paperWeapon = new Weapon("Paper");
+        let scissorsWeapon = new Weapon("Scissors");
+        let lizzardWeapon = new Weapon("Lizzard");
+        let spockWeapon = new Weapon("Spock");
+
+    
+    
+    }
+
+
+
+displayInformation() {
+    console.log(this.name);
+    this.displayRules();
+
+    displayRules() 
+        console.log("Welcome to the War Dice Game!")
+        console.log("Two players will roll 6 dice and total up their results.")
+        console.log("The higher total will earn that player a point");
+        console.log("The first player to three points will win the game!");
+    
+    
+}
+}
+
+
+
+
+
+
+
+console.log(Weapon);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////////////////////////////////////
+////////////////////////////////////////////////
+
+// // outputs
+// Rock crushes Scissors  
+// Scissors cuts Paper 
+// Paper covers Rock 
+// Rock crushes Lizard 
+// Lizard poisons Spock 
+// Spock smashes Scissors 
+// Scissors decapitates Lizard 
+// Lizard eats Paper 
+// Paper disproves Spock 
+// Spock vaporizes Rock  
--- rpsls.js
+++ rpsls.js
@@ -15,20 +15,20 @@
 
 // start with player vs player then automate "player 2"
 
-// classes - player, computer, weapon, game
+// classes - player, computer, gesture, game
 
 
 
 
-//What if you use weapon array as if player 1 calls [0] and playeer 2 calls [3] player2 ++ points 
+//What if you use gesture array as if player 1 calls [0] and playeer 2 calls [3] player2 ++ points 
 ////////////////////////////////////////////////
 ///////////////////////////////////////////////
 
 
 class Game {
     constructor() {
-        this.playerOne = new Player("Test");
-
+        this.playerOne = new Player("Test1");
+        this.playerTwo = new Player("Test2");
         
 
     }
@@ -51,44 +51,114 @@ class Game {
     playersThrowGestures(){
         this.chooseGesture();
     }
-    chooseGesture(){
-        let gestureChoice = ""
 
-        gestureChoice = prompt("Please chose a gesture to throw.");
-
-        
-    }
 
 
 }
         
-class Player {
+class Player extends Game{
     constructor(name) {
         this.score = 0;
         this.name = name;
-        this.weaponSet = [];
+        this.gestureThrow = [];
 
-        this.weaponSet.push(new Weapon("Rock"));
-        this.weaponSet.push(new Weapon("Paper"));
-        this.weaponSet.push(new Weapon("Scissors"));
-        this.weaponSet.push(new Weapon("Lizard"));
-        this.weaponSet.push(new Weapon("Spock"));
+        this.gestureThrow.push(new Gesture("Rock"));
+        this.gestureThrow.push(new Gesture("Paper"));
+        this.gestureThrow.push(new Gesture("Scissors"));
+        this.gestureThrow.push(new Gesture("Lizard"));
+        this.gestureThrow.push(new Gesture("Spock"));
     }
+    
+    chooseGesture(){
+        let gestureChoiceOne = ""
+
+        gestureChoiceOne = prompt("Player one, please chose a gesture to throw out of Rock, Paper, Scissors, Lizard or Spock.");
+
+
+        let gestureChoiceTwo = ""
+
+        gestureChoiceTwo = prompt("Player two, please chose a gesture to throw out of Rock, Paper, Scissors, Lizard or Spock.");
+        
+    }
+
+            if (gestureChoiceOne === "Scissors" && gestureChoiceTwo === "Rock");
+            console.log("Player Two Wins!! Rock Crushes Scissors!!");
+        
+            if (gestureChoiceOne === "Paper" && gestureChoiceTwo === "Scissors");
+            console.log("Player Two Wins!! Scissors Cuts Paper!!");
+
+            if (gestureChoiceOne === "Rock" && gestureChoiceTwo === "Paper");
+            console.log("Player Two Wins!! Paper Covers Rock!!");
+
+            if (gestureChoiceOne === "Lizard" && gestureChoiceTwo === "Rock");
+            console.log("Player Two Wins!! Rock Crushes Lizard!!");
+
+            if (gestureChoiceOne === "Spock" && gestureChoiceTwo === "Lizard");
+            console.log("Player Two Wins!! Lizard Poisons Spock!!");
+
+            if (gestureChoiceOne === "Scissors" && gestureChoiceTwo === "Spock");
+            console.log("Player Two Wins!! Spock Smashes Scissors!!");
+
+            if (gestureChoiceOne === "Lizard" && gestureChoiceTwo === "Scissors");
+            console.log("Player Two Wins!! Scissors Decapitates Lizard!!");
+
+            if (gestureChoiceOne === "Paper" && gestureChoiceTwo === "Lizard");
+            console.log("Player Two Wins!! Lizard Eats Paper!!");
+
+            if (gestureChoiceOne === "Spock" && gestureChoiceTwo === "Paper");
+            console.log("Player Two Wins!! Paper Disproves Spock!!");
+
+            if (gestureChoiceOne === "Rock" && gestureChoiceTwo === "Spock");
+            console.log("Player Two Wins!! Spock Vaporizes Rock!!");
+
+
+
+            if (gestureChoiceOne === "Rock" && gestureChoiceTwo === "Scissors");
+            console.log("Player One Wins!! Rock Crushes Scissors!!");
+        
+            if (gestureChoiceOne === "Scissors" && gestureChoiceTwo === "Paper");
+            console.log("Player One Wins!! Scissors Cuts Paper!!");
+
+            if (gestureChoiceOne === "Paper" && gestureChoiceTwo === "Rock");
+            console.log("Player One Wins!! Paper Covers Rock!!");
+
+            if (gestureChoiceOne === "Rock" && gestureChoiceTwo === "Lizard");
+            console.log("Player One Wins!! Rock Crushes Lizard!!");
+
+            if (gestureChoiceOne === "Lizard" && gestureChoiceTwo === "Spock");
+            console.log("Player One Wins!! Lizard Poisons Spock!!");
+
+            if (gestureChoiceOne === "Spock" && gestureChoiceTwo === "Scissors");
+            console.log("Player One Wins!! Spock Smashes Scissors!!");
+
+            if (gestureChoiceOne === "Scissors" && gestureChoiceTwo === "Lizard");
+            console.log("Player One Wins!! Scissors Decapitates Lizard!!");
+
+            if (gestureChoiceOne === "Lizard" && gestureChoiceTwo === "Paper");
+            console.log("Player One Wins!! Lizard Eats Paper!!");
+
+            if (gestureChoiceOne === "Paper" && gestureChoiceTwo === "Spock");
+            console.log("Player One Wins!! Paper Disproves Spock!!");
+
+            if (gestureChoiceOne === "Spock" && gestureChoiceTwo === "Rock");
+            console.log("Player One Wins!! Spock Vaporizes Rock!!");
 }
-class Human{
+
+
+class Human extends Player{
     constructor() {
         this.score = 0;
     }
 }
-class Computer {
+class Computer extends Player{
     constructor() {
         this.score = 0;
     }
 }
 
-class Weapon {
-    constructor(weaponName) {
-        this.weaponName = weaponName;
+class Gesture extends Player{
+    constructor(gestureName) {
+        this.gestureName = gestureName;
     }
 }
 
@@ -100,7 +170,7 @@ game.runGame();
 
 // console.log('game', game);
 
-// console.log(player.weaponSet[0].weaponName);
+// console.log(player.gestureThrow[0].gestureName);
 
 
 
